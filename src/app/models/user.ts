dd1b0f620c6d68a2784a648845558969ceb30065
Deepak Solanki <deepak.solanki2335@gmail.com>
2024-10-07 10:11:48 +0530

    Add the model of user

---
+++ src/app/models/user.ts
@@ -0,0 +1,70 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+// Message Interface
+export interface Message extends Document {
+    content: string;
+    createdAt: Date;
+}
+
+// Message Schema
+const messageSchema = new Schema<Message>({
+    content: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        required: true,
+        default: Date.now
+    }
+});
+
+// User Interface
+export interface User extends Document {
+    username: string;
+    email: string;
+    password: string;
+    verifyCode: string;
+    verifyCodeExpiry: Date;
+    isAcceptingMessage: boolean;
+    messages: Message[];
+}
+
+// User Schema
+const userSchema: Schema<User> = new Schema({
+    username: {
+        type: String,
+        required: [true, "Username is required"],
+        unique: true
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        unique: true,
+        match: [new RegExp("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$"), "Please use a valid email address"]
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"],
+        minlength: 6,
+        maxlength: 30
+    },
+    verifyCode: {
+        type: String,
+        default: null
+    },
+    verifyCodeExpiry: {
+        type: Date,
+        default: null
+    },
+    isAcceptingMessage: {
+        type: Boolean,
+        default: true
+    },
+    messages: [messageSchema]  // Embedding message schema
+});
+
+// Check if model already exists, else create it
+const UserModel = mongoose.models.User || mongoose.model<User>("User", userSchema);
+
+export default UserModel;
